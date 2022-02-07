c7f9fee656b33ace007f069c8e9312299b0b0843
Dmytro Gorbunov <dmytro.gorbunov@gmail.com>
2022-02-07 14:28:07 +0100

    Allow login via IPv6, change image to Rhel

--- ipv6/make_spec.py
+++ ipv6/make_spec.py
@@ -42,7 +42,9 @@ template = Template("""{
 
 print (template.render(
               user_data = base64.b64encode(ud.read()).decode("ascii"), 
-              image_id = 'ami-06d8dc7de2805917d',
+              # image_id = 'ami-06d8dc7de2805917d', - amazon linux 2, arm
+              image_id = 'ami-0f387ed70a4960190', # RHEL 8.3 BYOL              RHEL-8.3.0_HVM_BETA-20200701-arm64-2-Access2-GP2"
+
               instance_profile =  kv['InstanceProfileIPV6'],
               subnet_id = kv['SubnetId2'],
               security_group_id = kv['InstanceSecurityGroup'],
